6bb4f3f4db6d10bd9f1b239f373d4333abc62d56
gab885 <gabriellegault@hotmail.com>
2024-11-06 15:17:43 -0500

    add bias and updated train method

--- src/perceptron.py
+++ src/perceptron.py
@@ -16,8 +16,10 @@ class Perceptron:
         self.hiddenLayerSize = 3
 
         self.W1 = np.random.randn(self.inputLayerSize,self.hiddenLayerSize)
+        self.b1 = np.ones((self.hiddenLayerSize))
+
         self.W2 = np.random.randn(self.hiddenLayerSize,self.outputLayerSize)
-       
+        self.b2 = np.ones((self.outputLayerSize))
 
     def sigmoid(z):
       return 1/(1 + np.exp(-z))
@@ -49,63 +51,31 @@ class Perceptron:
         capDelta1 = np.dot(self.a1.T,delta2)
         capDelta2 = np.dot(self.a2.T,delta3)
 
-        m = Y.shape[0]
+        m = self.a1.shape[0]
 
         self.W1 = self.W1 - self.alpha* (capDelta1/m)
+        self.b2 = self.b2 - self.alpha * np.sum(delta3, axis=0)
         self.W2 = self.W2 - self.alpha* (capDelta2/m)
+        self.b1 = self.b1 - self.alpha * (1./m) * np.sum(delta2, axis=0)
 
     def loss(self, a3, Y):
        m = Y.shape[0]
        loss = (1/m)*np.sum(np.multiply(Y, np.log(a3)) + np.multiply((1-Y), np.log(1-a3)))
        return loss      
 
-     #fonction tiré du document Perceptron.ipynb fournis
-    def train(self,X,Y, n, eta, epsilon, training_images, training_labels):
-    
-         """  train_errors.append(0)
-        
-          for i in range(n):  
-              xi = training_images[i]
-              xi.resize(784, 1)
+    # Tiré du ficher FFNN.ipynb 
+    def train(self,X,Y):
+     for i in range(self.iters):
+            y_pred = self.forward_prog(self,X)
+            loss = self.loss(self,self.a3, y_pred)
             
-            # Compute activation for the example xi
-              score = np.dot(W,xi)
-            
-              best_label = score.argmax(axis=0)
-            
-              if(best_label != training_labels[i]):
-                  train_errors[epoch] = train_errors[epoch] + 1
-                
-          epoch = epoch + 1    
-        
-        # loop to update the weights
-          for i in range(n):
-              xi = training_images[i]
-              xi.resize(784, 1)
-            
-              u = np.array(u_function(np.dot(W,xi)))
-            
-              dxi = np.zeros((1,10)).T
-              dxi[training_labels[i]] = 1
-              dxi_minus_u = np.subtract(dxi, u)
-            
-              p = np.multiply(dxi_minus_u, np.transpose(xi))
-              W = W + eta * p
-            
-        # while loop terminates when algorithm converges or when number of epoch is 100
-        # when training data is quite high, eta is 1 and epsilon is 0 then
-        # there are high chances of algorithm not getting converged
-        # making while loop run infinitely, so max. epochs is set to 100
-          if(train_errors[epoch - 1]/n <= epsilon or epoch==100):
-              break """
-         for i in range(iter):
-            yPred = self.forward_prog(self,X)
-            loss = self.loss(self,yPred,Y )
+            self.backward_prog(self, Y)
 
-            self.backward_prog(self,Y)
-
-            #print something ... 
-
+            if i == 0 or i ==self.iters-1:
+                print(f"Iteration: {i+1}")
+                print(tabulate(zip(X, y, [np.round(y_pred) for y_pred in self.A3] ), headers=["Input", "Actual", "Predicted"]))
+                print(f"Loss: {loss}")                
+                print("\n")
          
        
        
